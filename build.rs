3e668e78d2d61821b096b3dfabedba1e5ee53c1d
Nex <callum.cunha@gmail.com>
2025-06-21 20:38:52 +0100

    fix: Improve frontend build handling and cleanup in build.rs; format vite.config.ts

--- build.rs
+++ build.rs
@@ -1,28 +1,37 @@
 use std::process::Command;
 
+const FRONTEND_BUILD_DIR: &str = "dist";
+
 fn main() {
-    if std::fs::exists("dist").is_ok_and(|v| v) {
-        println!("cargo:warning=Spa build detected, skipping build");
-        return
+    let is_release = std::env::var("PROFILE").is_ok_and(|v| v == "release");
+    let is_frontend_built = std::fs::exists(FRONTEND_BUILD_DIR).is_ok_and(|v| v);
+
+    match (is_release, is_frontend_built) {
+        (true, true) => {
+            println!("cargo:warning=Release build is forcing frontend rebuild");
+            std::fs::remove_dir_all(FRONTEND_BUILD_DIR).expect("deletion of frontend build failed");
+        }
+        (false, true) => {
+            println!("cargo:warning=Debug build is re-using frontend build");
+            return;
+        }
+        _ => println!("cargo:warning=Building frontend"),
     }
-    
-    let manager = js_package_manager();
-    println!("cargo:warning=Using {} as package manager", manager);
 
     // Run the build command
-    let status = Command::new(manager)
+    let status = Command::new(js_package_manager())
         .arg("run")
         .arg("build")
         .status()
-        .expect("Failed to execute build command");
+        .expect("frontend build failed with detected js package manager");
 
     if !status.success() {
-        panic!("Build command failed with status: {}", status);
+        panic!("Frontend build command failed with status: {}", status);
     }
 }
 
 fn js_package_manager() -> &'static str {
-    ["pnpm", "bun", "deno", "yarn", "npm"]
+    let manager = ["pnpm", "bun", "deno", "yarn", "npm"]
         .iter()
         .find_map(|manager| {
             Command::new(manager)
@@ -31,5 +40,7 @@ fn js_package_manager() -> &'static str {
                 .map(|_| manager)
                 .ok()
         })
-        .expect("no JavaScript package manager installation found")
+        .expect("no JavaScript package manager installation found");
+    println!("cargo:warning=Using {} as package manager", manager);
+    manager
 }
